5c789d3b74e1c7bba4f6d94ad109956558bb11f3
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-07-24 10:15:01 -0700

    make gemini-1.5-flash works

--- gemini-pro.js
+++ gemini-pro.js
@@ -1,13 +1,33 @@
 import dotenv from 'dotenv'
+import fetch from 'node-fetch'
 import { GoogleGenerativeAI } from '@google/generative-ai'
 
 dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY)
+// const weahterApiKey = process.env.OPENWEATHER_API_KEY;
+// const city = 'Seattle';
+// const url =     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+// async function getWeather(){
+//     try{
+//         const response = await fetch(url);
+//         const data = await response.json();
+
+//         if (response.ok){
+//             const temprature = data.main.temp;
+//             const weatherDescription = data.weahter[0].description;
+//         }
+//     }
+    
+
+
+// }
+
 
 async function run(){
     const model = genAI.getGenerativeModel({model: "gemini-pro"});
-    const prompt = "How is the Weather today?!";
+    const prompt = "what's the weather in Seattle in July18th 2024";
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
@@ -17,3 +37,4 @@ async function run(){
 }
 
 run();
+
